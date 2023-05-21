f8e212fa3439b13550ee4c4d414532ac881be6c8
Dipankar Halder <dipankar15-11137@diu.edu.bd>
2023-05-21 18:34:05 +0600

    seeds complate

--- src/Components/Pages/Home/HomePage/Seeds/Seed.js
+++ src/Components/Pages/Home/HomePage/Seeds/Seed.js
@@ -1,225 +1,42 @@
 import React from "react";
 import "../../../../CSS/ImageStyle.css";
 
-const Seed = () => {
+const Seed = ({ product, index }) => {
+  const { name, img, price, description } = product;
   return (
-    <div className="grid grid-cols-4 gap-5">
-      <div
-        data-aos="fade-up"
-        data-aos-offset="200"
-        data-aos-delay="60"
-        data-aos-duration="1200"
-        data-aos-easing="ease-in-out"
-        className="card w-96  shadow-xl hover:shadow-2xl bg-pink-50 "
-      >
-        <figure>
+    <div
+      data-aos="fade-up"
+      data-aos-offset="200"
+      data-aos-delay="60"
+      data-aos-duration="1200"
+      data-aos-easing="ease-in-out"
+      className="card w-96  shadow-xl hover:shadow-2xl bg-pink-50 "
+    >
+      <figure>
+        {index % 2 === 0 ? (
           <img
-            className="w-full h-62 origin-bottom -rotate-12 pic-style cursor-pointer"
-            src="https://www.thespruce.com/thmb/PfKjmre81C58M2OVidjZQmVwqPc=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/how-long-do-vegetable-seeds-last-1403089-1.1-9285f5dd00704eecbe8a436b5ba3d9c1.jpg"
+            className="w-full h-[270px] origin-bottom -rotate-12 pic-style cursor-pointer"
+            src={img}
             alt=""
           />
-        </figure>
-        <div className="card-body hover:bg-rose-100">
-          <h2 className="card-title">Seeds!</h2>
-          <p>
-            Price : <span className="font-bold">$ 5</span>
-          </p>
-          <p>
-            In botany, seed is an undeveloped plant embryo and food reserve
-            enclosed in a protective outer covering called a seed coat.{" "}
-          </p>
-          <div className="card-actions justify-end">
-            <button className="btn btn-primary hover:animate-pulse">
-              Buy Now
-            </button>
-          </div>
-        </div>
-      </div>
-
-      <div
-        data-aos="fade-up"
-        data-aos-offset="200"
-        data-aos-delay="60"
-        data-aos-duration="1200"
-        data-aos-easing="ease-in-out"
-        className="card w-96 bg-pink-50 shadow-xl hover:shadow-2xl"
-      >
-        <figure>
+        ) : (
           <img
-            className="w-full h-62 skew-y-12 pic-style cursor-pointer"
-            src="https://img.freepik.com/free-photo/sack-rice-seed-with-white-rice-small-wooden-spoon-rice-plant_1150-35744.jpg"
+            className="w-full h-[270px] origin-bottom skew-y-12 pic-style cursor-pointer"
+            src={img}
             alt=""
           />
-        </figure>
-        <div className="card-body">
-          <h2 className="card-title">Seeds</h2>
-          <p>
-            Price : <span className="font-bold">$ 5</span>
-          </p>
-          <p>
-            In botany, seed is an undeveloped plant embryo and food reserve
-            enclosed in a protective outer covering called a seed coat.{" "}
-          </p>
-          <div className="card-actions justify-end">
-            <button className="btn btn-primary hover:animate-pulse">
-              Buy Now
-            </button>
-          </div>
-        </div>
-      </div>
-      <div
-        data-aos="fade-up"
-        data-aos-offset="200"
-        data-aos-delay="60"
-        data-aos-duration="1200"
-        data-aos-easing="ease-in-out"
-        className="card w-96 bg-pink-50 shadow-xl hover:shadow-2xl"
-      >
-        <figure>
-          <img
-            className="w-full h-64 origin-bottom -rotate-12 pic-style cursor-pointer"
-            src="https://www.theseedcollection.com.au/assets/thumb/X-035.jpg?20201210130421"
-            alt=""
-          />
-        </figure>
-        <div className="card-body">
-          <h2 className="card-title">Seeds!</h2>
-          <p>
-            Price : <span className="font-bold">$ 5</span>
-          </p>
-          <p>
-            In botany, seed is an undeveloped plant embryo and food reserve
-            enclosed in a protective outer covering called a seed coat.{" "}
-          </p>
-          <div className="card-actions justify-end">
-            <button className="btn btn-primary hover:animate-pulse">
-              Buy Now
-            </button>
-          </div>
-        </div>
-      </div>
-      <div
-        data-aos="fade-up"
-        data-aos-offset="200"
-        data-aos-delay="60"
-        data-aos-duration="1200"
-        data-aos-easing="ease-in-out"
-        className="card w-96 bg-pink-50 shadow-xl hover:shadow-2xl"
-      >
-        <figure>
-          <img
-            className="w-full h-62 skew-y-12 pic-style cursor-pointer"
-            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTvTm9phX9LMq4olJgmpXJD49MN0KbR8s5r2w&usqp=CAU"
-            alt=""
-          />
-        </figure>
-        <div className="card-body">
-          <h2 className="card-title">Seeds</h2>
-          <p>
-            Price : <span className="font-bold">$ 5</span>
-          </p>
-          <p>
-            In botany, seed is an undeveloped plant embryo and food reserve
-            enclosed in a protective outer covering called a seed coat.{" "}
-          </p>
-          <div className="card-actions justify-end">
-            <button className="btn btn-primary hover:animate-pulse">
-              Buy Now
-            </button>
-          </div>
-        </div>
-      </div>
-      <div
-        data-aos="fade-up"
-        data-aos-offset="200"
-        data-aos-delay="60"
-        data-aos-duration="1200"
-        data-aos-easing="ease-in-out"
-        className="card w-96 bg-pink-50 shadow-xl hover:shadow-2xl"
-      >
-        <figure>
-          <img
-            className="w-full h-62 origin-bottom -rotate-12 pic-style cursor-pointer"
-            src="https://www.espoma.com/wp-content/uploads/2017/08/dried-vegetable-seeds.jpg"
-            alt=""
-          />
-        </figure>
-        <div className="card-body">
-          <h2 className="card-title">Seeds!</h2>
-          <p>
-            Price : <span className="font-bold">$ 5</span>
-          </p>
-          <p>
-            In botany, seed is an undeveloped plant embryo and food reserve
-            enclosed in a protective outer covering called a seed coat.{" "}
-          </p>
-          <div className="card-actions justify-end">
-            <button className="btn btn-primary hover:animate-pulse">
-              Buy Now
-            </button>
-          </div>
-        </div>
-      </div>
-      <div
-        data-aos="fade-up"
-        data-aos-offset="200"
-        data-aos-delay="60"
-        data-aos-duration="1200"
-        data-aos-easing="ease-in-out"
-        className="card w-96 bg-pink-50 shadow-xl hover:shadow-2xl"
-      >
-        <figure>
-          <img
-            className="w-full h-62 skew-y-12 pic-style cursor-pointer"
-            src="https://www.gardentech.com/-/media/project/oneweb/gardentech/images/blog/starting-seeds-right-in-your-garden/seed_packets_01.jpg"
-            alt=""
-          />
-        </figure>
-        <div className="card-body">
-          <h2 className="card-title">Seeds</h2>
-          <p>
-            Price : <span className="font-bold">$ 5</span>
-          </p>
-          <p>
-            In botany, seed is an undeveloped plant embryo and food reserve
-            enclosed in a protective outer covering called a seed coat.{" "}
-          </p>
-          <div className="card-actions justify-end">
-            <button className="btn btn-primary hover:animate-pulse">
-              Buy Now
-            </button>
-          </div>
-        </div>
-      </div>
-      <div
-        data-aos="fade-up"
-        data-aos-offset="200"
-        data-aos-delay="60"
-        data-aos-duration="1200"
-        data-aos-easing="ease-in-out"
-        className="card w-96 bg-pink-50 shadow-xl hover:shadow-2xl"
-      >
-        <figure>
-          <img
-            className="w-full h-62 origin-bottom -rotate-12 pic-style  cursor-pointer"
-            src="https://www.realsimple.com/thmb/QyUdARPmprZwvWiw3FtW8OBIBB8=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/buy-vegetable-seeds-online-2000-33199f5dafc84b648f93473207f62eca.jpg"
-            alt=""
-          />
-        </figure>
-        <div className="card-body">
-          <h2 className="card-title">Seeds!</h2>
-          <p>
-            Price : <span className="font-bold">$ 5</span>
-          </p>
-          <p>
-            In botany, seed is an undeveloped plant embryo and food reserve
-            enclosed in a protective outer covering called a seed coat.{" "}
-          </p>
-          <div className="card-actions justify-end">
-            <button className="btn btn-primary hover:animate-pulse">
-              Buy Now
-            </button>
-          </div>
+        )}
+      </figure>
+      <div className="card-body hover:bg-rose-100">
+        <h2 className="card-title">{name}!</h2>
+        <p>
+          Price : <span className="font-bold">$ {price}</span>
+        </p>
+        <p>{description}</p>
+        <div className="card-actions justify-end">
+          <button className="btn btn-primary hover:animate-pulse">
+            Buy Now
+          </button>
         </div>
       </div>
     </div>
--- src/Components/Pages/Home/HomePage/Seeds/Seeds.js
+++ src/Components/Pages/Home/HomePage/Seeds/Seeds.js
@@ -1,13 +1,25 @@
-import React from "react";
-import Seed from "./Seed";
+import React, { useEffect, useState } from 'react';
+import Seed from './Seed';
 
 const Seeds = () => {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    const url = `http://localhost:5000/product/OUR SEEDS`;
+    fetch(url)
+      .then(res => res.json())
+      .then(data => setProducts(data));
+  }, []);
   return (
     <div className="mx-20 bg-slate-50 p-2 mt-12 rounded-lg">
       <h1 className="text-center pt-4 mb-16 text-4xl font-extrabold uppercase">
         Our Seeds
       </h1>
-      <Seed />
+
+      <div className="grid grid-cols-4 gap-5">
+        {products.map((product, index) => (
+          <Seed key={product._id} product={product} index={index}></Seed>
+        ))}
+      </div>
     </div>
   );
 };
