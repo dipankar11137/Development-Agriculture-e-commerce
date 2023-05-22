fa1c9f2a003a21fc45e3d277083ee73e40466a04
Dipankar Halder <dipankar15-11137@diu.edu.bd>
2023-05-23 02:15:17 +0600

    edit my item

--- src/Components/Pages/MyItem/MyItems.js
+++ src/Components/Pages/MyItem/MyItems.js
@@ -1,10 +1,61 @@
-import React from "react";
-import MyItem from "./MyItem";
+import React, { useEffect, useState } from 'react';
+import { toast } from 'react-toastify';
+import MyItem from './MyItem';
 
 const MyItems = () => {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    const url = `http://localhost:5000/bookProducts`;
+    fetch(url)
+      .then(res => res.json())
+      .then(data => setProducts(data));
+  }, [products]);
+
+  const handleDelete = id => {
+    const proceed = window.confirm('Are You Sure ?');
+    if (proceed) {
+      const url = `http://localhost:5000/bookProducts/${id}`;
+      fetch(url, {
+        method: 'DELETE',
+      })
+        .then(res => res.json())
+        .then(data => {
+          const remaining = products.filter(product => product._id !== id);
+          setProducts(remaining);
+          toast.success('Delete  ');
+        });
+    }
+  };
   return (
-    <div>
-      <MyItem />
+    <div className="mb-32">
+      <div className="overflow-x-auto">
+        <table className="table table-compact w-full">
+          <thead className="text-center">
+            <tr>
+              <th>Index</th>
+              <th></th>
+              <th>Name</th>
+              <th>Category</th>
+              <th>Quantity</th>
+              <th>Date</th>
+              <th>Price</th>
+              <th>Total Price</th>
+              <th className="pl-48">Payment</th>
+              <th className="pl-28">Delete</th>
+            </tr>
+          </thead>
+          <tbody className="text-center">
+            {products.map((product, index) => (
+              <MyItem
+                key={product._id}
+                product={product}
+                index={index + 1}
+                handleDelete={handleDelete}
+              ></MyItem>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
